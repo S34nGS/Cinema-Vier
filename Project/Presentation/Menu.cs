3b0e69831879fc625049331facbf1e2d6c76585f
Arczius <damianvaartmans@gmail.com>
2026-03-26 15:41:16 +0100

    added a base menu

--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -4,25 +4,10 @@ static class Menu
     //This shows the menu. You can call back to this method to show the menu again
     //after another presentation method is completed.
     //You could edit this to show different menus depending on the user's role
+    static string header = "Welcome to Cinema Vier! Please select an option:";
     static public void Start()
     {
-        Console.WriteLine("Enter 1 to login");
-        Console.WriteLine("Enter 2 to do something else in the future");
-
-        string input = Console.ReadLine();
-        if (input == "1")
-        {
-            UserLogin.Start();
-        }
-        else if (input == "2")
-        {
-            Console.WriteLine("This feature is not yet implemented");
-        }
-        else
-        {
-            Console.WriteLine("Invalid input");
-            Start();
-        }
-
+        List<string> menu = ["View Movies", "Login", "Register", "Exit"];
+        int selected = UiLib.SelectionMenu(menu, header);
     }
 }
