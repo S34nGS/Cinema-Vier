bdbe280de8b11faf56b919d37ab60ecb6ef2c960
Arczius <damianvaartmans@gmail.com>
2026-03-26 16:26:59 +0100

    added some logic needed for the user login and creation

--- Project/Logic/AccountsLogic.cs
+++ Project/Logic/AccountsLogic.cs
@@ -12,16 +12,31 @@ public class AccountsLogic
 
     public AccountsLogic()
     {
-        // Could do something here
 
     }
+
+    public string HashPassword(string password)
+    {
+        return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(password));
+    }
+
+    public AccountModel CreateAccount(string email, string password, string fullName)
+    {
+        AccountModel account = new AccountModel(0, email, HashPassword(password), fullName);
+
+        _access.Write(account);
+
+        account = _access.GetByEmail(email);
+        return account;
+    }
+
 
     public AccountModel CheckLogin(string email, string password)
     {
 
 
         AccountModel acc = _access.GetByEmail(email);
-        if (acc != null && acc.Password == password)
+        if (acc != null && acc.Password == HashPassword(password))
         {
             CurrentAccount = acc;
             return acc;
@@ -29,7 +44,3 @@ public class AccountsLogic
         return null;
     }
 }
-
-
-
-
