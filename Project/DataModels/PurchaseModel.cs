8f2770ddafb849ec23db16239f0fc635030cf6ba
dzhao735-cloud <dzhao735@gmail.com>
2026-04-03 14:11:33 +0200

    made the properties private set

--- Project/DataModels/PurchaseModel.cs
+++ Project/DataModels/PurchaseModel.cs
@@ -1,9 +1,9 @@
 public class PurchaseModel
 {
-    public int? UserId { get; set; }
-    public int TimetableId { get; set; }
-    public DateTime SelectedDateTime { get; set; }
-    public string PaymentMethod { get; set; }
+    public int? UserId { get; private set; }
+    public int TimetableId { get; private set; }
+    public DateTime SelectedDateTime { get; private set; }
+    public string PaymentMethod { get; private set; }
 
     public PurchaseModel (int? userId, int timetableId, DateTime selectedDateTime, string paymentMethod)
     {
