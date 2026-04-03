627b85bf5bdfb77e539f700c4c3759fd8e3ccb71
dzhao735-cloud <dzhao735@gmail.com>
2026-04-03 14:23:38 +0200

    deleted the timetable property, we dont need this right now

--- Project/DataModels/PurchaseModel.cs
+++ Project/DataModels/PurchaseModel.cs
@@ -1,14 +1,14 @@
 public class PurchaseModel
 {
     public int? UserId { get; private set; }
-    public int TimetableId { get; private set; }
     public DateTime SelectedDateTime { get; private set; }
     public string PaymentMethod { get; private set; }
+    // public int TimetableId { get; private set; }
+    // public int SeatId { get; private set;}
 
-    public PurchaseModel (int? userId, int timetableId, DateTime selectedDateTime, string paymentMethod)
+    public PurchaseModel (int? userId, DateTime selectedDateTime, string paymentMethod)
     {
         UserId = userId;
-        TimetableId = timetableId;
         SelectedDateTime = selectedDateTime;
         PaymentMethod = paymentMethod;
     }
