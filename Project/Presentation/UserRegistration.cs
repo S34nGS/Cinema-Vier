d980e960fceff3371fe18e8485a2b73301132832
Arczius <damianvaartmans@gmail.com>
2026-04-08 18:39:06 +0200

    added account creation visualization on the register page

--- Project/Presentation/UserRegistration.cs
+++ Project/Presentation/UserRegistration.cs
@@ -8,9 +8,19 @@ static class UserRegistration
         List<string> fields = ["Full Name", "Email", "Password"];
         Dictionary<string, string> inputs = UiLib.InputForm(fields, "Please enter your registration information");
         AccountModel acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password"], inputs["Full Name"]);
+        string errorMessage = null;
+        while(acc == null)
+        {
+            errorMessage = "Account couldn't be created";
+
+            inputs = UiLib.InputForm(inputs, "Please enter your registration information", header: errorMessage);
+            acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password"], inputs["Full Name"]);
+
+        }
+
         if (acc != null)
         {
-            Console.WriteLine("Welcome back " + acc.FullName);
+            Console.WriteLine("Welcome " + acc.FullName);
             Console.WriteLine("Your email number is " + acc.EmailAddress);
 
             Menu.Start();
