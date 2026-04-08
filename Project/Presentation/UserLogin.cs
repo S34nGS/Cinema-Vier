b325e4599f605c258378ea8df987fe30f5439ed0
Damian Vaartmans <damianvaartmans@gmail.com>
2026-04-08 18:35:55 +0200

    added validation for login

--- Project/Presentation/UserLogin.cs
+++ Project/Presentation/UserLogin.cs
@@ -7,7 +7,29 @@ static class UserLogin
     {
         List<string> fields = ["Email", "Password"];
         Dictionary<string, string> inputs = UiLib.InputForm(fields, "Please enter your login information");
-        AccountModel acc = accountsLogic.CheckLogin(inputs["Email"], inputs["Password"]);
+        AccountModel? acc = accountsLogic.CheckLogin(inputs["Email"], inputs["Password"]);
+        string? errorMessage = null;
+
+        if (acc == null)
+        {
+            errorMessage = "No account found with that email and password";
+        }
+
+        while(acc == null)
+        {
+            inputs = UiLib.InputForm(inputs, "Please enter your login information", header: errorMessage);
+            acc = accountsLogic.CheckLogin(inputs["Email"], inputs["Password"]);
+
+             if (acc == null)
+            {
+                errorMessage = "No account found with that email and password";
+            }
+        }
+
+
+        Console.WriteLine("Welcome back " + acc.FullName);
+        UiLib.HoldUser();
+
         if (acc != null)
         {
             Console.WriteLine("Welcome back " + acc.FullName);
