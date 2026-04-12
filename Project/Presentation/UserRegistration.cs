43ff542ce06e8ac76fc9032b38c17d3ee9ffd975
Arczius <damianvaartmans@gmail.com>
2026-04-12 13:18:59 +0200

    changed some structure around in UserRegistration

--- Project/Presentation/UserRegistration.cs
+++ Project/Presentation/UserRegistration.cs
@@ -7,8 +7,8 @@ static class UserRegistration
     {
         List<string> fields = ["Full Name", "Email", "Password"];
         Dictionary<string, string> inputs = UiLib.InputForm(fields, "Please enter your registration information");
-        AccountModel acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password"], inputs["Full Name"]);
-        string errorMessage = null;
+        AccountModel? acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password"], inputs["Full Name"]);
+        string errorMessage;
         while(acc == null)
         {
             errorMessage = "Account couldn't be created";
@@ -18,16 +18,9 @@ static class UserRegistration
 
         }
 
-        if (acc != null)
-        {
-            Console.WriteLine("Welcome " + acc.FullName);
-            Console.WriteLine("Your email number is " + acc.EmailAddress);
-
-            Menu.Start();
-        }
-        else
-        {
-            Console.WriteLine("Account couldn't be created");
-        }
+        Console.WriteLine("Account created successfully");
+        UiLib.HoldUser();
+        
+        Menu.Start();
     }
 }
