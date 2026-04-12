7f8adff6ba3b6e544d65a7d6aa13db91e97834ca
Arczius <damianvaartmans@gmail.com>
2026-04-12 13:15:36 +0200

    removed unused code

--- Project/Presentation/UserLogin.cs
+++ Project/Presentation/UserLogin.cs
@@ -8,34 +8,18 @@ static class UserLogin
         List<string> fields = ["Email", "Password"];
         Dictionary<string, string> inputs = UiLib.InputForm(fields, "Please enter your login information");
         AccountModel? acc = accountsLogic.CheckLogin(inputs["Email"], inputs["Password"]);
-        string? errorMessage = null;
+        string? errorMessage;
 
         while(acc == null)
         {
             errorMessage = "No account found with that email and password";
             inputs = UiLib.InputForm(inputs, "Please enter your login information", header: errorMessage);
             acc = accountsLogic.CheckLogin(inputs["Email"], inputs["Password"]);
-
-            if (acc == null)
-            {
-                errorMessage = "No account found with that email and password";
-            }
         }
 
 
         Console.WriteLine("Welcome back " + acc.FullName);
         UiLib.HoldUser();
-
-        if (acc != null)
-        {
-            Console.WriteLine("Welcome back " + acc.FullName);
-            Console.WriteLine("Your email number is " + acc.EmailAddress);
-
-            Menu.Start();
-        }
-        else
-        {
-            Console.WriteLine("No account found with that email and password");
-        }
+        Menu.Start();
     }
 }
