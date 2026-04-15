be339987b0b751968411f24313847205299d7012
S34nGS <steenhuis20@gmail.com>
2026-04-15 13:59:35 +0200

    Finished search functionality

--- Project/Presentation/MoviesMenu.cs
+++ Project/Presentation/MoviesMenu.cs
@@ -1,9 +1,17 @@
+using System.Linq;
+
 static class MoviesMenu
 {
     public static string header = "All available movies:";
-    public static void Start()
+    public static MovieModel Start()
     {
-        int selected = UiLib.SelectionMenu(MoviesLogic.GetMovieTitles(), header);
-        // System.Console.WriteLine(MoviesLogic.GetMovieData(selected));
+        int selected = UiLib.SelectionMenu(MoviesLogic.GetMovieTitles().Prepend("Search").ToList(), header);
+        if (selected == 0)
+        {
+            string input = UiLib.Input("", header);
+            int selectedSearch = UiLib.SelectionMenu(MoviesLogic.GetByPartOfTitle(input), header);
+            return MoviesLogic.GetMovieData(selectedSearch);
+        }
+        return MoviesLogic.GetMovieData(selected - 1);
     }
 }
