8bce6396a9b694b64e294c876733046914d61f77
dzhao735-cloud <dzhao735@gmail.com>
2026-04-15 15:22:30 +0200

    the type of userid is from int? changed to Int64?

--- Project/DataModels/PurchaseModel.cs
+++ Project/DataModels/PurchaseModel.cs
@@ -1,12 +1,12 @@
 public class PurchaseModel
 {
-    public int? UserId { get; private set; }
+    public Int64? UserId { get; private set; }
     public DateTime SelectedDateTime { get; private set; }
     public string PaymentMethod { get; private set; }
     // public int TimetableId { get; private set; }
     // public int SeatId { get; private set;}
 
-    public PurchaseModel (int? userId, DateTime selectedDateTime, string paymentMethod)
+    public PurchaseModel (Int64? userId, DateTime selectedDateTime, string paymentMethod)
     {
         UserId = userId;
         SelectedDateTime = selectedDateTime;
