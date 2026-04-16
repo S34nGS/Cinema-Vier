2a6fabaf239c81ca9acdf98ecde387c4799479f7
dzhao735-cloud <dzhao735@gmail.com>
2026-04-16 12:03:02 +0200

    a few comments added to PurchaseModel, ignore this.

--- Project/DataModels/PurchaseModel.cs
+++ Project/DataModels/PurchaseModel.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class PurchaseModel
 {
     public Int64? UserId { get; private set; }
@@ -5,11 +7,14 @@ public class PurchaseModel
     public string PaymentMethod { get; private set; }
     // public int TimetableId { get; private set; }
     // public int SeatId { get; private set;}
+    // [ForeignKey("Movie")]
+    // public int MovieId { get; set; }
 
     public PurchaseModel (Int64? userId, DateTime selectedDateTime, string paymentMethod)
     {
         UserId = userId;
         SelectedDateTime = selectedDateTime;
         PaymentMethod = paymentMethod;
+        // MovieId = movieId;
     }
 }
