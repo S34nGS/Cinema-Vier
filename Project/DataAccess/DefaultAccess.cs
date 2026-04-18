c71421ca82eab9aa7077433baa7da1db4f1dd55a
mais33 <maissukkari1997@gmail.com>
2026-04-18 03:07:34 +0200

    Update DefaultAccess.cs

--- Project/DataAccess/DefaultAccess.cs
+++ Project/DataAccess/DefaultAccess.cs
@@ -2,6 +2,7 @@ using Microsoft.Data.Sqlite;
 
 public abstract class DefaultAccess
 {
+    protected SqliteConnection connection = new SqliteConnection("Data Source=DataSources/project.db");
     protected abstract string Table { get; }
 
     protected abstract void CreateTable();
