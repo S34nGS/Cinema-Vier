7ea5c11353b92fc1d584c861870f1ed1eed8a63c
Jasarat0617 <jknl0331@gmail.com>
2026-04-19 14:20:44 +0200

    Add password indicator for length

--- Project/Presentation/UserRegistration.cs
+++ Project/Presentation/UserRegistration.cs
@@ -4,16 +4,16 @@ static class UserRegistration
 
     public static void Start()
     {
-        List<string> fields = ["Full Name", "Email", "Password"];
-        Dictionary<string, string> inputs = UiLib.InputForm(fields, "Please enter your registration information (Password must be at least 8 characters)");
-        AccountModel? acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password"], inputs["Full Name"]);
+        List<string> fields = ["Full Name", "Email", "Password (8-32 characters)"];
+        Dictionary<string, string> inputs = UiLib.InputForm(fields, "Please enter your registration information");
+        AccountModel? acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password (8-32 characters)"], inputs["Full Name"]);
         string errorMessage;
         while(acc == null)
         {
             errorMessage = "Account couldn't be created";
 
-            inputs = UiLib.InputForm(inputs, "Please enter your registration information (Password must be at least 8 characters)", header: errorMessage);
-            acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password"], inputs["Full Name"]);
+            inputs = UiLib.InputForm(inputs, "Please enter your registration information", header: errorMessage);
+            acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password (8-32 characters)"], inputs["Full Name"]);
 
         }
 
