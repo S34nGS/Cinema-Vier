8a06351336924e26662c53cffc0c2d87a15701a8
dzhao735-cloud <dzhao735@gmail.com>
2026-04-21 00:31:44 +0200

    changed table type INTEGER to TEXT of dateOfBirth

--- Project/DataAccess/AccountsAccess.cs
+++ Project/DataAccess/AccountsAccess.cs
@@ -13,7 +13,7 @@ public class AccountsAccess : DefaultAccess
                 email TEXT UNIQUE NOT NULL, 
                 password TEXT NOT NULL, 
                 fullname TEXT NOT NULL,
-                dateOfBirth INTEGER NOT NULL
+                dateOfBirth TEXT NOT NULL
             )";
         connection.Execute(sql);
     }
