c54c88977c10c9cb5aa415533cc1323d0bc5ede0
dzhao735-cloud <dzhao735@gmail.com>
2026-04-21 00:34:18 +0200

    argument dateOfBirth added when creating account in userregistration

--- Project/Presentation/UserRegistration.cs
+++ Project/Presentation/UserRegistration.cs
@@ -6,14 +6,16 @@ static class UserRegistration
     {
         List<string> fields = ["Full Name", "Email", "Password (8-32 characters)", "Date of birth (dd/mm/yyyy)"];
         Dictionary<string, string> inputs = UiLib.InputForm(fields, "Please enter your registration information");
-        AccountModel? acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password (8-32 characters)"], inputs["Full Name"], inputs["Date of birth (dd/mm/yyyy)"]);
+        DateTime.TryParseExact(inputs["Date of birth (dd/mm/yyyy)"], "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime dateOfBirth);
+        AccountModel? acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password (8-32 characters)"], inputs["Full Name"], dateOfBirth);
         string errorMessage;
         while(acc == null)
         {
             errorMessage = "Account couldn't be created";
 
             inputs = UiLib.InputForm(inputs, "Please enter your registration information", header: errorMessage);
-            acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password (8-32 characters)"], inputs["Full Name"]);
+            DateTime.TryParseExact(inputs["Date of birth (dd/mm/yyyy)"], "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime dateofBirth);
+            acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password (8-32 characters)"], inputs["Full Name"], dateofBirth);
 
         }
 
