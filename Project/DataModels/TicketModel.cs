3f1679d6837c34a5df0a7c773c6b30e57ce17a4f
dzhao735-cloud <dzhao735@gmail.com>
2026-04-20 21:47:28 +0200

    int64 id is added to ticketmodel

--- Project/DataModels/TicketModel.cs
+++ Project/DataModels/TicketModel.cs
@@ -2,14 +2,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 public class TicketModel
 {
+    public Int64 Id { get; set; }
     public Int64? UserId { get; private set; }
     public DateTime SelectedDateTime { get; private set; }
     public string PaymentMethod { get; private set; }
     // public int TimetableId { get; private set; }
     // public int SeatId { get; private set;}
 
-    public TicketModel (Int64? userId, DateTime selectedDateTime, string paymentMethod)
+    public TicketModel (Int64? Id, Int64? userId, DateTime selectedDateTime, string paymentMethod)
     {
+        Id = Id;
         UserId = userId;
         SelectedDateTime = selectedDateTime;
         PaymentMethod = paymentMethod;
