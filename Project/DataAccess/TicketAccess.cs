dd9b83e2306872c78acfea2e873d2334c0823e43
dzhao735-cloud <dzhao735@gmail.com>
2026-04-20 12:00:20 +0200

    TicketAccess class created, foreignkey is added

---
+++ Project/DataAccess/TicketAccess.cs
@@ -0,0 +1,55 @@
+using Dapper;
+
+
+public class TicketAccess : DefaultAccess
+{
+    protected override string Table { get; } = "Tickets";
+    public override void CreateTable()
+    {
+        string sql = $@"CREATE TABLE IF NOT EXISTS {Table} 
+            (
+                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                price INTEGER NOT NULL,
+                reservationId INTEGER NOT NULL,
+                seatId INTEGER NOT NULL,
+                movieId INTEGER NOT NULL,
+                FOREIGN KEY (reservationid) REFERENCES ReservationId(id), 
+                FOREIGN KEY (seatid) REFERENCES SeatId(id), 
+                FOREIGN KEY (movieid) REFERENCES Movies(id)
+            )";
+        connection.Execute(sql);
+    }
+
+    public void Write(TicketModel ticket)
+    {
+        string sql = $@"INSERT INTO {Table} 
+            (reservationId, seatId, price, movieId)
+            VALUES (@ReservationId, @SeatId, @Price, @MovieId)";
+        connection.Execute(sql, ticket);
+    }
+
+    public AccountModel GetByReservationId(int id)
+    {
+        string sql = $"SELECT * FROM {Table} WHERE reservationId = @ReservationId";
+        return connection.QueryFirstOrDefault<AccountModel>(sql, new { Id = id });
+    }
+
+    public AccountModel GetBySeatId(int id)
+    {
+        string sql = $"SELECT * FROM {Table} WHERE seatId = @SeatId";
+        return connection.QueryFirstOrDefault<AccountModel>(sql, new { Id = id });
+    }    
+
+    public void Update(TicketModel ticket)
+    {
+        string sql =
+            $"UPDATE {Table} SET reservationId = @ReservationId, seatId = @SeatId, price = @Price, movieId = @MovieId WHERE id = @Id";
+        connection.Execute(sql, ticket);
+    }
+
+    public void Delete(TicketModel ticket)
+    {
+        string sql = $"DELETE FROM {Table} WHERE id = @Id";
+        connection.Execute(sql, new { Id = ticket.Id });
+    }
+}
