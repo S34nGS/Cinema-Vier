eda8a401fd7a9a01778ae50a09bb198eab9a91f7
S34nGS <steenhuis20@gmail.com>
2026-04-22 13:34:26 +0200

    changed header after login

--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -4,9 +4,9 @@ static class Menu
     //This shows the menu. You can call back to this method to show the menu again
     //after another presentation method is completed.
     //You could edit this to show different menus depending on the user's role
-    static string header = "Welcome to Cinema Vier! Please select an option:";
     static public void Start()
     {
+        string header = (AccountsLogic.CurrentAccount != null) ? $"Welcome {AccountsLogic.CurrentAccount.FirstName}" : "Welcome to Cinema Vier! Please select an option:";
         List<string> menu = ["View Movies", "Login", "Register", "View Reservations", "Cinema Info", "Exit"];
         int selected = UiLib.SelectionMenu(menu, header, true);
 
@@ -27,7 +27,7 @@ static class Menu
                     Start();
                     return;
                 }
-              
+
                 if (AccountsLogic.CurrentAccount != null)
                 {
                     if (!MoviesLogic.IsOldEnough(movie, AccountsLogic.CurrentAccount))
