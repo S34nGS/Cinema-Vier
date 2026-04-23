e50a2d335afd8f9cf3dd65c79249223adb81f847
S34nGS <steenhuis20@gmail.com>
2026-04-23 14:26:37 +0200

    made beginning changes

--- Project/Presentation/MoviesMenu.cs
+++ Project/Presentation/MoviesMenu.cs
@@ -3,38 +3,54 @@ using System.Linq;
 static class MoviesMenu
 {
     public static string header = "All available movies:";
+    // public static int Start()
+    // {
+    //     while (true)
+    //     {
+    //         int selected = UiLib.SelectionMenu(MoviesLogic.GetMovieTitles().Prepend("Search").ToList(), header);
+            
+    //         if (selected == -1) return -1;
+
+    //         if (selected == 0)
+    //         {
+    //             while (true)
+    //             {
+    //                 string input = UiLib.Input("Search movie:");
+    //                 if (input == "-1") break;
+    //                 List<string> searchedMovieList = MoviesLogic.GetByPartOfTitle(input);
+    //                 if (searchedMovieList.Count == 0)
+    //                 {
+    //                     UiLib.SelectionMenu(
+    //                         ["No movies found."],
+    //                         "Results",
+    //                         true
+    //                     );
+    //                     continue;
+    //                 }
+    //                 int selectedSearch = UiLib.SelectionMenu(searchedMovieList, header);
+    //                 if (selectedSearch == -1) continue;
+    //                 return selectedSearch;
+    //             }
+    //             continue;
+    //         }
+            
+    //         return selected - 1;
+    //     }
+    // }
+
     public static int Start()
     {
-        while (true)
-        {
-            int selected = UiLib.SelectionMenu(MoviesLogic.GetMovieTitles().Prepend("Search").ToList(), header);
-            
-            if (selected == -1) return -1;
+        return Search();
+    }
 
-            if (selected == 0)
-            {
-                while (true)
-                {
-                    string input = UiLib.Input("Search movie:");
-                    if (input == "-1") break;
-                    List<string> searchedMovieList = MoviesLogic.GetByPartOfTitle(input);
-                    if (searchedMovieList.Count == 0)
-                    {
-                        UiLib.SelectionMenu(
-                            ["No movies found."],
-                            "Results",
-                            true
-                        );
-                        continue;
-                    }
-                    int selectedSearch = UiLib.SelectionMenu(searchedMovieList, header);
-                    if (selectedSearch == -1) continue;
-                    return selectedSearch;
-                }
-                continue;
-            }
-            
-            return selected - 1;
-        }
+    public static int Search()
+    {
+        // int selected = UiLib.SelectionMenu("Search".ToList(), header);
+        return UiLib.SelectionMenu(MoviesLogic.GetMovieTitles(), header);
+    }
+
+    public static int MovieList()
+    {
+        return UiLib.SelectionMenu(MoviesLogic.GetMovieTitles(), header);
     }
 }
