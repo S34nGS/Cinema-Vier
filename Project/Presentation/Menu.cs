4a0822149d3211aba98d3c3726b85c3c50e49442
S34nGS <steenhuis20@gmail.com>
2026-05-06 14:04:50 +0200

    fix main menu option

--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -8,7 +8,7 @@ static class Menu
         string header = (AccountsLogic.CurrentAccount != null)
             ? $"Welcome {AccountsLogic.CurrentAccount.FirstName}"
             : "Welcome to Cinema Vier! Please select an option:";
-        List<string> menu = ["View Movies", "Login", "Register", "View Reservations", "Cinema Info", "Exit"];
+        List<string> menu = ["Order Movie", "Login", "Register", "View Reservations", "Cinema Info", "Exit"];
         int selected = UiHelper.SelectionMenu(menu, header, true);
 
         if (selected == menu.IndexOf("Login"))
