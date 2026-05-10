579688c5fc5737c966f1ba7ae6d2e534fcb4ef29
Damian Vaartmans <damianvaartmans@tuta.io>
2026-05-11 00:11:21 +0200

    updated the seatmodel to be properties

--- Project/DataModels/SeatModel.cs
+++ Project/DataModels/SeatModel.cs
@@ -1,7 +1,10 @@
 public class SeatModel
 {
-
-    public Int64 Id, RoomId, Row, SeatNumber, SeatPriority;
+    public Int64 Id {get; set;}
+    public Int64 RoomId {get; set;}
+    public Int64 Row {get; set;}
+    public Int64 SeatNumber {get; set;}
+    public Int64 SeatPriority {get; set;}
 
     public SeatModel(Int64 id, Int64 roomId, Int64 row, Int64 seatNumber, Int64 seatPriority)
     {
