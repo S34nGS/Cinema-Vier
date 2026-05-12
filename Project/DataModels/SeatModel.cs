9bc039c849eb19f84574084e89df03b7f7d4f249
Damian Vaartmans <damianvaartmans@tuta.io>
2026-05-12 12:30:03 +0200

    updated the roommodel to have a available property

--- Project/DataModels/SeatModel.cs
+++ Project/DataModels/SeatModel.cs
@@ -5,6 +5,7 @@ public class SeatModel
     public Int64 Row {get; set;}
     public Int64 SeatNumber {get; set;}
     public Int64 SeatPriority {get; set;}
+    public bool Available;
 
     public SeatModel(Int64 id, Int64 roomId, Int64 row, Int64 seatNumber, Int64 seatPriority)
     {
@@ -13,5 +14,16 @@ public class SeatModel
         Row = row;
         SeatNumber = seatNumber;
         SeatPriority = seatPriority;
+        Available = true;
+    }
+
+    public SeatModel(Int64 id, Int64 roomId, Int64 row, Int64 seatNumber, Int64 seatPriority, Int64 available)
+    {
+        Id = id;
+        RoomId = roomId;
+        Row = row;
+        SeatNumber = seatNumber;
+        SeatPriority = seatPriority;
+        Available = available !> 0;
     }
 }
