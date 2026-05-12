39c06dd1304d1c006602d5455b10a40bbda4c1f9
Damian Vaartmans <damianvaartmans@tuta.io>
2026-05-12 16:45:41 +0200

    added some functionality to the seat selection menu

--- Project/Presentation/SeatSelection.cs
+++ Project/Presentation/SeatSelection.cs
@@ -1,22 +1,71 @@
 public static class SeatSelection
 {
     private static SeatLogic _logic = new();
-    public static void Start()
+    public static void Start(int roomId = 1)
     {
-        Console.Clear();
-        List<SeatModel> seats = _logic.GetSeatsByRoomId(1);
+
+        List<SeatModel> seats = _logic.GetSeatsByRoomId(roomId);
+        Dictionary<string, int> Coordinates = new();
+        Coordinates["x"] = 1;
+        Coordinates["y"] = 1;
+        bool has_active_input = true;
+        do
+        {
+            Console.Clear();
+
+            WriteScreen(seats, Coordinates);
+            ConsoleKey keyPressed = Console.ReadKey().Key;
+
+            if(keyPressed == ConsoleKey.H && Coordinates["x"] > 1)
+            {
+                Coordinates["x"]--;
+            }
+            else if(keyPressed == ConsoleKey.L)
+            {
+                Coordinates["x"]++;
+            }
+            else if(keyPressed == ConsoleKey.J)
+            {
+                Coordinates["y"]++;
+            }
+            else if(keyPressed == ConsoleKey.K)
+            {
+                Coordinates["y"]--;
+            }
+        } while(has_active_input);
+
+    }
+
+    public static void WriteScreen(List<SeatModel> seats, Dictionary<string, int> Coordinates)
+    {
         Int64 current_row = 1;
 
-        Console.WriteLine("-- Screen --");
         foreach(SeatModel seat in seats)
         {
             if(seat.Row > current_row)
             {
-                Console.Write(Environment.NewLine);
+                Console.WriteLine();
+                Console.WriteLine();
                 current_row = seat.Row;
             }
 
-            Console.Write("□ ");
+            if(seat.SeatNumber == 1)
+            {
+                Console.Write(seat.Row < 10 ? $" {seat.Row}" : seat.Row); 
+                Console.Write("  ");
+            }
+            if(Coordinates["x"] == seat.SeatNumber && Coordinates["y"] == seat.Row)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("█");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.Write("░");
+            }
+            Console.Write(" ");
         }
+        Console.WriteLine();
     }
 }
