dcffeaffee212dbcc15d98925d849211c271411a
S34nGS <steenhuis20@gmail.com>
2026-05-13 08:21:40 +0200

    fixed back button issue for disable movie

--- Project/Presentation/DisableMovie.cs
+++ Project/Presentation/DisableMovie.cs
@@ -4,6 +4,7 @@ public static class DisableMovie
     {
         List<string> movies = MoviesLogic.GetMovieTitles();
         int selectedMovie = UiHelper.SelectionMenu(movies);
+        if (selectedMovie == -1) return;
 
         MovieModel movie = MoviesLogic.GetMovieByTitle(movies[selectedMovie]);
         movie.IsActive = 0;
