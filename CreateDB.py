92d5fd08658bd5abd46da7823fe9f7b731b3032b
Arczius <damianvaartmans@gmail.com>
2026-05-19 13:27:51 +0200

    improved the run scripts slightly

--- CreateDB.py
+++ CreateDB.py
@@ -1,13 +1,14 @@
 #! /usr/bin/env python3
-
 import os
 import subprocess
+
 
 def CreateDB():
     if os.name == "nt":
         subprocess.run(r'dotnet run --project ".\CreateDB\\"', shell=True)
     else:
         subprocess.run("dotnet run --project './CreateDB'", shell=True)
+
 
 if __name__ == "__main__":
     if os.path.exists("./Project/DataSources/project.db"):
