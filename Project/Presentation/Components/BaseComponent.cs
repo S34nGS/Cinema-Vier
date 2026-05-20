9f5bb4c8763fd56cc8baad0590cfde2c15519926
Damian Vaartmans <damianvaartmans@tuta.io>
2026-05-20 13:34:00 +0200

    made the data components need into a base component so all components can inherit it

--- Project/Presentation/Components/BaseComponent.cs
+++ Project/Presentation/Components/BaseComponent.cs
@@ -1,7 +1,33 @@
 public abstract class BaseComponent
 {
+    public readonly ContinueAndBackMenu continueAndBackMenu = new();
+
     public static int GetLongestString(IEnumerable<string> strings)
     {
         return strings.Max(x => x.Length);
+    }
+
+    private static void WriteHeader(string? header)
+    {
+        if (!string.IsNullOrEmpty(header))
+        {
+            Console.WriteLine(header);
+        }
+    }
+
+    public static string ShowInput(string input, string title)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return "";
+        }
+        else if (title.Contains("password", StringComparison.OrdinalIgnoreCase))
+        {
+            return new string('*', input.Length);
+        }
+        else
+        {
+            return input;
+        }
     }
 }
