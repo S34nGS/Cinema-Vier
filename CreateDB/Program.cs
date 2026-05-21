21325291d50f9d26026569d5aae13463e5d8fd9e
Damian Vaartmans <damianvaartmans@tuta.io>
2026-05-21 07:40:10 +0200

    fixed the typing on the CreateDB script

--- CreateDB/Program.cs
+++ CreateDB/Program.cs
@@ -192,12 +192,12 @@ public static class Program
 
         // added default menu items in CreateDB
         List<MenuItemModel> menuItemsList = [
-            new MenuItemModel(0, "Popcorn", "Snack", 2.00m),
-            new MenuItemModel(0, "Nachos", "Snack", 3.50m),
-            new MenuItemModel(0, "Chips", "Snack", 1.50m),
-            new MenuItemModel(0, "Water", "Drink", 1.00m),
-            new MenuItemModel(0, "Cola", "Drink", 2.00m),
-            new MenuItemModel(0, "Juice", "Drink", 2.50m)
+            new MenuItemModel(0, "Popcorn", "Snack", 2.00),
+            new MenuItemModel(0, "Nachos", "Snack", 3.50),
+            new MenuItemModel(0, "Chips", "Snack", 1.50),
+            new MenuItemModel(0, "Water", "Drink", 1.00),
+            new MenuItemModel(0, "Cola", "Drink", 2.00),
+            new MenuItemModel(0, "Juice", "Drink", 2.50)
         ];
 
         foreach (MenuItemModel item in menuItemsList)
