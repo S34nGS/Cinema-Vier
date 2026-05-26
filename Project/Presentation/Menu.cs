09ed6a7fea16c6329d54488730abed2209500588
S34nGS <steenhuis20@gmail.com>
2026-05-26 20:21:03 +0200

    added booking movie as admin option to menu

--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -16,7 +16,7 @@ static class Menu
         }
         else if (AccountsLogic.CurrentAccount.IsAdmin == 1)
         {
-            menu = ["Add Movie", "Edit Movie", "Disable Movie", "Manage Timetables", "Logout"];
+            menu = ["Book Movie For Customer", "Add Movie", "Edit Movie", "Disable Movie", "Manage Timetables", "Logout"];
         }
         else
         {
@@ -91,6 +91,11 @@ static class Menu
         else if (selected == menu.IndexOf("Manage Timetables"))
         {
             Timetables.Start();
+            Start();
+        }
+        else if (selected == menu.IndexOf("Book Movie For Customer"))
+        {
+            AdminBookMovie.Start();
             Start();
         }
         else if (selected == menu.IndexOf("Logout"))
