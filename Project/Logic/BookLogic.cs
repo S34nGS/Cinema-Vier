c878f56ecee95bdc99d0873cf4658fc6042045be
S34nGS <steenhuis20@gmail.com>
2026-05-26 20:19:13 +0200

    added logic file for bookings

---
+++ Project/Logic/BookLogic.cs
@@ -0,0 +1,34 @@
+public static class BookLogic
+{
+    public static void BookAsAdmin(string header)
+    {
+        AccountsLogic accountsLogic = new();
+        AccountModel account = accountsLogic.GetCustomerAsAdmin(header);
+
+        while (true)
+        {
+            MovieModel? movie = MoviesLogic.Start();
+
+            PurchaseTicket.SetUpDateMenu(movie);
+            UiHelper.HoldUser(movie.ToString());
+            if (!MoviesLogic.IsOldEnough(movie, account))
+            {
+                    UiHelper.HoldUser($"You must be {movie.AgeRating}+ to watch this movie.");
+                    BookAsAdmin(header);
+            }
+
+            while (true)
+            {
+                TicketModel? purchaseTicket = PurchaseTicket.Start(movie, account);
+                if (purchaseTicket is null)
+                {
+                    break;
+                }
+                else
+                {
+                    Menu.Start();
+                }
+            }
+        }
+    }
+}
