42ecf36bbd11ab5b9760001bbe9d558b6897b678
Arczius <damianvaartmans@gmail.com>
2026-05-26 14:31:47 +0200

    added a reference to seats in the ReservationModel

--- Project/DataModels/ReservationModel.cs
+++ Project/DataModels/ReservationModel.cs
@@ -5,19 +5,20 @@ public class ReservationModel
     public Int64 ReservationDate { get; set; }
     public double TotalPrice { get; set; }
     public Int64 TimeTableId { get; set; }
-    public Int64 SeatId { get; set; }
+    public List<SeatModel> Seats { get; set; } = [];
 
-    public ReservationModel()
+
+    public ReservationModel(Int64 id, Int64 userId, Int64 reservationDate, double totalPrice, Int64 timeTableId, List<SeatModel> seats) : this(id, userId, reservationDate, totalPrice, timeTableId)
     {
-        
+        Seats = seats;
     }
-    public ReservationModel(Int64 id, Int64 userId, Int64 reservationDate, double totalPrice, Int64 timeTableId, Int64 seatId)
+
+    public ReservationModel(Int64 id, Int64 userId, Int64 reservationDate, double totalPrice, Int64 timeTableId)
     {
         Id = id;
         UserId = userId;
         ReservationDate = reservationDate;
         TotalPrice = totalPrice;
         TimeTableId = timeTableId;
-        SeatId = seatId;
     }
 }
