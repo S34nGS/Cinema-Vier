56d2973e53ae64185891611cc83c7747e8aed309
dzhao735-cloud <dzhao735@gmail.com>
2026-05-31 01:44:30 +0200

    passpoints added to the accountmodel

--- Project/DataModels/AccountModel.cs
+++ Project/DataModels/AccountModel.cs
@@ -12,9 +12,9 @@
     public Int64 DateOfBirth { get; set; }
     public Int64 IsAdmin { get; set; }
     public Int64 FreePopcornGiftUsedYear { get; set; }
+    public Int64 PassPoints { get; set; }
 
-
-    public AccountModel(Int64 id, string email, string password, string firstName, string lastName, Int64 dateOfBirth, Int64 isAdmin = 0, Int64 freePopcornGiftUsedYear = 0)
+    public AccountModel(Int64 id, string email, string password, string firstName, string lastName, Int64 dateOfBirth, Int64 isAdmin = 0, Int64 freePopcornGiftUsedYear = 0, Int64 passPoints = 1)
     {
         Id = id;
         EmailAddress = email;
@@ -25,6 +25,7 @@
         DateOfBirth = dateOfBirth;
         IsAdmin = isAdmin;
         FreePopcornGiftUsedYear = freePopcornGiftUsedYear;
+        PassPoints = passPoints;
     }
 
     public AccountModel() { }
