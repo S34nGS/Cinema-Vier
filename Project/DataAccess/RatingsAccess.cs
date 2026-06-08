7b05f2ec10b725a07df797d9e1e22c82bc3dadab
S34nGS <steenhuis20@gmail.com>
2026-06-08 23:07:30 +0200

    created basics for ratings

---
+++ Project/DataAccess/RatingsAccess.cs
@@ -0,0 +1,28 @@
+using Dapper;
+
+public class RatingsAccess : DefaultAccess, IAccess
+{
+    public static string Table { get; } = "Rating";
+
+    public override void CreateTable()
+    {
+        string sql = $@"CREATE TABLE IF NOT EXISTS {Table} (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            userId INTEGER NOT NULL,
+            movieId INTEGER NOT NULL,
+            rating REAL NOT NULL,
+
+            FOREIGN KEY (movieId) REFERENCES Movie(id)
+            FOREIGN KEY (userId) REFERENCES Account(id)
+        );";
+        connection.Execute(sql);
+    }
+
+    public void Write(RatingModel rating)
+    {
+        string sql = $@"INSERT INTO {Table}
+            (userId, movieId, rating) 
+            VALUES (@UserId, @MovieId, @Rating)";
+        connection.Execute(sql, rating);
+    }
+}
