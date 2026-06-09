0effce5b460f20fa0bb923aa28ba067a9dc4d3ff
dzhao735-cloud <dzhao735@gmail.com>
2026-06-09 23:50:46 +0200

    method start changed of class UserRegistration

--- Project/Presentation/UserRegistration.cs
+++ Project/Presentation/UserRegistration.cs
@@ -9,25 +9,19 @@
 
         DateTime.TryParseExact(inputs["Date of birth (dd/mm/yyyy)"], "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime dateOfBirth);
 
-        AccountModel? acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password (8-32 characters)"], inputs["First Name"], inputs["Last Name"], dateOfBirth);
-
-        string errorMessage;
+        var (acc, result) = accountsLogic.CreateAccount(inputs["Email"], inputs["Password (8-32 characters)"], inputs["First Name"], inputs["Last Name"], dateOfBirth);
 
         while (acc == null)
         {
-            errorMessage = "Account couldn't be created";
+            inputs = UiHelper.InputFormMenu.WriteMenu(inputs, "Please enter your registration information", header: GetErrorMessage(result));
 
-            inputs = UiHelper.InputFormMenu.WriteMenu(inputs, "Please enter your registration information", header: errorMessage);
+            DateTime.TryParseExact(inputs["Date of birth (dd/mm/yyyy)"], "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out dateOfBirth);
 
-            DateTime.TryParseExact(inputs["Date of birth (dd/mm/yyyy)"], "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime dateofBirth);
-
-            acc = accountsLogic.CreateAccount(inputs["Email"], inputs["Password (8-32 characters)"], inputs["First Name"], inputs["Last Name"], dateofBirth);
-
+            (acc, result) = accountsLogic.CreateAccount(inputs["Email"], inputs["Password (8-32 characters)"], inputs["First Name"], inputs["Last Name"], dateOfBirth);
         }
 
         Console.WriteLine("Account created successfully");
         UiHelper.HoldUser();
-
         Menu.Start();
     }
 
